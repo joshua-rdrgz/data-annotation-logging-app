93b8e611318e6f98d11554a6b21727182e1e4e04
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-11-19 14:34:13 -0600

    Refactor to use API service

--- src/pages/api/daily-logs.ts
+++ src/pages/api/daily-logs.ts
@@ -1,28 +1,33 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import prisma from '@/lib/prisma';
 import { DailyLog } from '@prisma/client';
+import { dailyLogAPIService } from '@/services/server/dailyLog';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === 'POST') {
-    try {
-      const dailyLog = await createDailyLog(JSON.parse(req.body));
-      res.status(200).json({
-        status: 'success',
-        data: dailyLog,
-      });
-    } catch (error: any) {
-      console.log('error: ', error);
-      res.status(500).json({
-        status: 'error',
-        message: `Something went wrong: ${error.message}`,
-      });
-    }
-  }
-}
+  let data: DailyLog | DailyLog[];
 
-async function createDailyLog(data: DailyLog) {
-  return await prisma.dailyLog.create({ data });
+  try {
+    switch (req.method) {
+      case 'GET':
+        data = await dailyLogAPIService.getDailyLogs();
+        break;
+      case 'POST':
+        data = await dailyLogAPIService.createDailyLog(req.body);
+        break;
+      default:
+        throw new Error('This route only supports GET and POST requests.');
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      status: 'error',
+      message: `Something went wrong on server: ${error.message}`,
+    });
+  }
 }
