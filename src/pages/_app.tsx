030dc7fc152c3669adeaa00b41e49455975e4316
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-11-19 14:35:07 -0600

    Add React Query SSR fetching

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,20 @@
+import { type AppProps } from 'next/app';
+import { HydrationBoundary } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AppProviders } from '@/context/AppProviders';
 import { AppLayout } from '@/ui/custom/AppLayout';
-import { type AppProps } from 'next/app';
+
 import './globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AppProviders>
-      <AppLayout>
-        <Component {...pageProps} />
-      </AppLayout>
+      <HydrationBoundary state={pageProps.dehydratedState}>
+        <AppLayout>
+          <Component {...pageProps} />
+        </AppLayout>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </HydrationBoundary>
     </AppProviders>
   );
 }
--- src/pages/daily-logs/index.tsx
+++ src/pages/daily-logs/index.tsx
@@ -1,24 +1,34 @@
-import type { InferGetStaticPropsType } from 'next';
-import prisma from '@/lib/prisma';
+import { QueryClient, dehydrate, useQuery } from '@tanstack/react-query';
+import { dailyLogClientService } from '@/services/client/dailyLog';
 import { CreateDailyLog } from '@/features/daily-logs/CreateDailyLog';
 import { DailyLogsTable } from '@/features/daily-logs/DailyLogsTable';
 
 export const getStaticProps = async () => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['daily-logs'],
+    queryFn: dailyLogClientService.getDailyLogs,
+  });
+
   return {
     props: {
-      dailyLogs: await prisma.dailyLog.findMany(),
+      dehydratedState: dehydrate(queryClient),
     },
   };
 };
 
-export default function DailyLogsPage({
-  dailyLogs,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function DailyLogsPage() {
+  const { data: dailyLogs } = useQuery({
+    queryKey: ['daily-logs'],
+    queryFn: dailyLogClientService.getDailyLogs,
+  });
+
   return (
-    <div>
+    <>
       <h1>DailyLogs Page</h1>
-      <DailyLogsTable data={dailyLogs} />
+      <DailyLogsTable data={dailyLogs || []} />
       <CreateDailyLog />
-    </div>
+    </>
   );
 }
