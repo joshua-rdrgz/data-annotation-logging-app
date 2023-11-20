61400b38e97fb35bbba9e230644ee6a22739aeb6
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-11-19 20:39:32 -0600

    Move log creation form to Dialog Closes #3

--- src/features/logs/CreateLog.tsx
+++ src/features/logs/CreateLog.tsx
@@ -1,91 +1,24 @@
-import { useForm } from 'react-hook-form';
-import { useMutation } from '@tanstack/react-query';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { toast } from 'react-hot-toast';
-import { QueryKeys } from '@/lib/queryKeys';
-import { useQueryClientInstance } from '@/context/query-provider';
-import { logSchema, type LogSchema } from '@/schemas/logSchema';
-import { logClientService } from '@/services/client/log';
-import { LogItem } from '@/features/logs/LogItem';
-import { Form } from '@/ui/form';
-import { Input } from '@/ui/input';
+import { useState } from 'react';
+import { CreateLogForm } from '@/features/logs/CreateLogForm';
+import * as D from '@/ui/dialog';
+import { Dialog } from '@/ui/custom/Dialog';
 import { Button } from '@/ui/button';
 
-const INPUTS = [
-  {
-    name: 'projectName' as const,
-    label: 'Project Name',
-  },
-  {
-    name: 'tasksCompleted' as const,
-    label: 'Tasks Completed',
-    inputProps: {
-      type: 'number',
-    },
-  },
-  {
-    name: 'minutesWorked' as const,
-    label: 'Minutes Worked',
-    inputProps: {
-      type: 'number',
-    },
-  },
-  {
-    name: 'hourlyRate' as const,
-    label: 'Hourly Rate',
-    inputProps: {
-      type: 'number',
-    },
-  },
-];
-
 export const CreateLog = () => {
-  const formMethods = useForm<LogSchema>({
-    resolver: zodResolver(logSchema),
-    defaultValues: {
-      projectName: '',
-      tasksCompleted: 0,
-      minutesWorked: 0,
-      hourlyRate: 0,
-    },
-  });
-
-  const { queryClient } = useQueryClientInstance();
-
-  const { mutate: createLog, isPending } = useMutation({
-    mutationFn: logClientService.createLog,
-    onSuccess() {
-      toast.success('Log successfully created', {
-        icon: '🎉',
-      });
-      queryClient.invalidateQueries({ queryKey: [QueryKeys.LOGS] });
-      formMethods.reset();
-    },
-    onError(error) {
-      toast.error('Uh oh, something went wrong....', {
-        icon: '💥',
-      });
-      console.error('🔥 ERROR 🔥: ', error.message);
-    },
-  });
-
-  const onSubmit = formMethods.handleSubmit((values) => createLog(values));
+  const [open, setOpen] = useState(false);
 
   return (
-    <Form formMethods={formMethods} onSubmit={onSubmit} className='space-y-3'>
-      {INPUTS.map((input) => (
-        <Form.Field
-          key={input.name}
-          name={input.name}
-          disabled={isPending}
-          render={({ field: { onChange, value } }) => (
-            <LogItem onChange={onChange} value={value} label={input.label} />
-          )}
-        />
-      ))}
-      <Button asChild>
-        <Input type='submit' disabled={isPending} />
-      </Button>
-    </Form>
+    <Dialog
+      controlled
+      open={open}
+      onOpenChange={setOpen}
+      triggerContent={<Button className='ml-auto'>🔨 Add New Log</Button>}
+    >
+      <D.Header>
+        <D.Title>Add a New Log</D.Title>
+        <D.Description>Fill out the form to add a new work log.</D.Description>
+      </D.Header>
+      <CreateLogForm onFormSuccess={() => setOpen(false)} />
+    </Dialog>
   );
 };
---
+++ src/features/logs/CreateLogForm.tsx
@@ -0,0 +1,99 @@
+import { useForm } from 'react-hook-form';
+import { useMutation } from '@tanstack/react-query';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { toast } from 'react-hot-toast';
+import { QueryKeys } from '@/lib/queryKeys';
+import { useQueryClientInstance } from '@/context/query-provider';
+import { logSchema, type LogSchema } from '@/schemas/logSchema';
+import { logClientService } from '@/services/client/log';
+import { LogItem } from '@/features/logs/LogItem';
+import { Form } from '@/ui/form';
+import { Input } from '@/ui/input';
+import { Button } from '@/ui/button';
+import React from 'react';
+
+const INPUTS = [
+  {
+    name: 'projectName' as const,
+    label: 'Project Name',
+  },
+  {
+    name: 'tasksCompleted' as const,
+    label: 'Tasks Completed',
+    inputProps: {
+      type: 'number',
+    },
+  },
+  {
+    name: 'minutesWorked' as const,
+    label: 'Minutes Worked',
+    inputProps: {
+      type: 'number',
+    },
+  },
+  {
+    name: 'hourlyRate' as const,
+    label: 'Hourly Rate',
+    inputProps: {
+      type: 'number',
+    },
+  },
+];
+
+interface CreateLogFormProps {
+  onFormSuccess?(): void;
+}
+
+export const CreateLogForm: React.FC<CreateLogFormProps> = ({
+  onFormSuccess,
+}) => {
+  const formMethods = useForm<LogSchema>({
+    resolver: zodResolver(logSchema),
+    defaultValues: {
+      projectName: '',
+      tasksCompleted: 0,
+      minutesWorked: 0,
+      hourlyRate: 0,
+    },
+  });
+
+  const { queryClient } = useQueryClientInstance();
+
+  const { mutate: createLog, isPending } = useMutation({
+    mutationFn: logClientService.createLog,
+    onSuccess() {
+      toast.success('Log successfully created', {
+        icon: '🎉',
+      });
+      queryClient.invalidateQueries({ queryKey: [QueryKeys.LOGS] });
+      onFormSuccess?.();
+      formMethods.reset();
+    },
+    onError(error) {
+      toast.error('Uh oh, something went wrong....', {
+        icon: '💥',
+      });
+      console.error('🔥 ERROR 🔥: ', error.message);
+    },
+  });
+
+  const onSubmit = formMethods.handleSubmit((values) => createLog(values));
+
+  return (
+    <Form formMethods={formMethods} onSubmit={onSubmit} className='space-y-3'>
+      {INPUTS.map((input) => (
+        <Form.Field
+          key={input.name}
+          name={input.name}
+          disabled={isPending}
+          render={({ field: { onChange, value } }) => (
+            <LogItem onChange={onChange} value={value} label={input.label} />
+          )}
+        />
+      ))}
+      <Button asChild>
+        <Input type='submit' disabled={isPending} />
+      </Button>
+    </Form>
+  );
+};
