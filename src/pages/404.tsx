5288c0876d352c7e510be62c1b80e57c0c2c11f1
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-11-25 17:25:51 -0600

    Add custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+export default function NotFoundPage() {
+  return (
+    <div className=' h-[90vh] flex flex-col gap-10 items-center justify-center text-center'>
+      <h1 className='text-6xl font-bold'>This page wasn&apos;t found 😭</h1>
+      <h2 className='text-3xl font-medium'>
+        Please pick one of the pages from the navigation menu to continue using
+        the application.
+      </h2>
+    </div>
+  );
+}
